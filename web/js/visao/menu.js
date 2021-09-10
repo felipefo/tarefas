8dce5d3eef3cb95ce1f08288ca315bf909d1cd07
felipefo <felipefo@users.noreply.github.com>
2021-09-10 13:56:51 -0300

    Primeiro commit
    Primeiro commit. Layout inicial e menu com javascript

---
+++ web/js/visao/menu.js
@@ -0,0 +1,11 @@
+
+
+function adicionaMenu(){
+    document.getElementById("menu").innerHTML = 
+            '<a href="tarefas.html">Tarefas | </a>' +
+            '<a href="categoria.html">Categoria | </a>' +
+            '<a href="meusdados.html">Meus dados | </a>' +
+            '<a href="logout.html">Logout</a>';       
+}
+document.addEventListener("DOMContentLoaded", adicionaMenu);
+
