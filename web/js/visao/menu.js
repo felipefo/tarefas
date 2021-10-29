6bb672cc5791a9ae94d4d6bfec8e6bbe0cdfd70d
felipefo <felipefo@users.noreply.github.com>
2021-10-29 07:54:58 -0300

    Update menu.js

--- web/js/visao/menu.js
+++ web/js/visao/menu.js
@@ -1,11 +1,21 @@
 
 
-function adicionaMenu(){
-    document.getElementById("menu").innerHTML = 
+function adicionaMenu() {
+    document.getElementById("menu").innerHTML =
             '<a href="index.html">Tarefas | </a>' +
             '<a href="categoria.html">Categoria | </a>' +
             '<a href="meusdados.html">Meus dados | </a>' +
-            '<a href="logout.html">Logout</a>';       
+            '<a href="logout.html">Logout</a>';
 }
 document.addEventListener("DOMContentLoaded", adicionaMenu);
+
+
+
+function openNav() {
+    document.getElementById("menu").style.width = "200px";
+}
+function closeNav() {
+    document.getElementById("menu").style.width = "0";
+}
+
 
