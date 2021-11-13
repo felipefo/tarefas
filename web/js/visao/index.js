b55e9733586de60762eb4d348d162f70355b2322
felipefo <felipefo@users.noreply.github.com>
2021-11-13 09:04:21 -0300

    melhoria
    Melhoria

--- web/js/visao/index.js
+++ web/js/visao/index.js
@@ -1,18 +1,18 @@
 
 function carregaListaTarefas(tarefas) {
 
+    var tabelaTarefas = document.getElementById("lista_de_tarefas");
     for (var tarefa in tarefas) {
         console.log(JSON.stringify(tarefa));
-        var tabelaTarefas = document.getElementById("lista_de_tarefas");
-        var corpo = tabelaTarefas.getElementsByTagName('tbody')[0];  
+        var corpo = tabelaTarefas.getElementsByTagName('tbody')[0];
         var novaLinha = corpo.insertRow(corpo.rows.length);
         novaLinha.innerHTML = '<tr>'
-            +'<td>' + tarefas[tarefa]["descricao"] +'</td>'
-            +'<td class="hide_420px">' + tarefas[tarefa]['categoria'] + '</td>'
-            +'<td>' + tarefas[tarefa]['status'] + '</td>'
-            +'<td>'  +tarefas[tarefa]['data_criacao'] + '</td>'
-            +'<td><input type="button" value="Editar"/>' 
-            +'<input type="button" value="Remover"/></td>'
-        +'</tr>';
+                + '<td>' + tarefas[tarefa]["descricao"] + '</td>'
+                + '<td class="hide_420px">' + tarefas[tarefa]['categoria'] + '</td>'
+                + '<td>' + tarefas[tarefa]['status'] + '</td>'
+                + '<td>' + tarefas[tarefa]['data_criacao'] + '</td>'
+                + '<td><input type="button" value="Editar"/>'
+                + '<input type="button" value="Remover"/></td>'
+                + '</tr>';
     }
 }
